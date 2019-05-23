b7e65cc20d92125d1d2514531222806d3e9cbef1
Alexander Ternowy <alexander.ternowy@gmail.com>
2019-05-23 11:56:42 +0200

    blalbala

---
+++ NAA.Webservices/UniversityWebService.asmx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using NAA.Services;
+using NAA.Data;
+using NAA.Services.Services;
+
+namespace NAA.Webservices
+{
+    /// <summary>
+    /// Summary description for UniversityWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class UniversityWebService : System.Web.Services.WebService
+    {
+
+        private NAA.Services.Services.ApplicationServiceUniversity();
+
+        public UniversityWebService()
+        {
+
+        }
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
